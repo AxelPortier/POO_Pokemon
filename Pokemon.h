91592361c4baa66d1cc1677dbc8e1871c5489bc5
TheMagicTurtl <pitigliv@gmail.com>
2025-04-12 11:46:34 +0200

    Update Pokemon.h

--- Pokemon.h
+++ Pokemon.h
@@ -9,55 +9,400 @@
 #include <set>
 #include <string>
 #include <Windows.h>
+#include <unordered_map>
 
 
+class Type {
+public: //vu qu'on veut y avoir accès facilement
+	std::string nom; //nom du type (eau, feu...)
+	std::unordered_map<std::string, float> faiblesses;
+	//c'est juste un dico, la clef est un string et la valeurs un double
 
+	std::unordered_map<std::string, float> resistances;
+
+	Type(std::string n) : nom(n) {}
+
+	~Type() {}
+
+	virtual void defFaiblesses() = 0; //on va les override dans les ss classes
+	virtual void defResistances() = 0;
+
+};
+class TypeFeu : public Type {
+public:
+    TypeFeu() : Type("Feu") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Eau"] = 2.0; // on met à jour le dico des faiblesses avec le multiplicateur
+        faiblesses["Roche"] = 2.0;
+        faiblesses["Sol"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Plante"] = 0.5; // pareil avec les résistances
+        resistances["Glace"] = 0.5;
+        resistances["Insecte"] = 0.5;
+        resistances["Acier"] = 0.5;
+        resistances["Fée"] = 0.5;
+    }
+};
+
+class TypeEau : public Type {
+public:
+    TypeEau() : Type("Eau") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Plante"] = 2.0; // on met à jour le dico des faiblesses avec le multiplicateur
+        faiblesses["Electrik"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Feu"] = 0.5; // pareil avec les résistances
+        resistances["Eau"] = 0.5;
+        resistances["Glace"] = 0.5;
+        resistances["Acier"] = 0.5;
+    }
+};
+
+class TypePlante : public Type {
+public:
+    TypePlante() : Type("Plante") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Feu"] = 2.0;
+        faiblesses["Glace"] = 2.0;
+        faiblesses["Poison"] = 2.0;
+        faiblesses["Vol"] = 2.0;
+        faiblesses["Insecte"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Eau"] = 0.5;
+        resistances["Sol"] = 0.5;
+        resistances["Roche"] = 0.5;
+    }
+};
+
+class TypeElectrik : public Type {
+public:
+    TypeElectrik() : Type("Électrik") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Sol"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Vol"] = 0.5;
+        resistances["Acier"] = 0.5;
+        resistances["Électrik"] = 0.5;
+    }
+};
+
+class TypeGlace : public Type {
+public:
+    TypeGlace() : Type("Glace") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Feu"] = 2.0;
+        faiblesses["Combat"] = 2.0;
+        faiblesses["Roche"] = 2.0;
+        faiblesses["Acier"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Glace"] = 0.5;
+    }
+};
+
+class TypeCombat : public Type {
+public:
+    TypeCombat() : Type("Combat") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Vol"] = 2.0;
+        faiblesses["Psy"] = 2.0;
+        faiblesses["Fée"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Roche"] = 0.5;
+        resistances["Insecte"] = 0.5;
+        resistances["Ténèbres"] = 0.5;
+    }
+};
+
+class TypePoison : public Type {
+public:
+    TypePoison() : Type("Poison") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Sol"] = 2.0;
+        faiblesses["Psy"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Plante"] = 0.5;
+        resistances["Fée"] = 0.5;
+        resistances["Combat"] = 0.5;
+        resistances["Poison"] = 0.5;
+        resistances["Insecte"] = 0.5;
+    }
+};
+
+class TypeSol : public Type {
+public:
+    TypeSol() : Type("Sol") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Eau"] = 2.0;
+        faiblesses["Plante"] = 2.0;
+        faiblesses["Glace"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Poison"] = 0.5;
+        resistances["Roche"] = 0.5;
+    }
+};
+
+class TypeVol : public Type {
+public:
+    TypeVol() : Type("Vol") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Electrik"] = 2.0;
+        faiblesses["Glace"] = 2.0;
+        faiblesses["Roche"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Plante"] = 0.5;
+        resistances["Combat"] = 0.5;
+        resistances["Insecte"] = 0.5;
+    }
+};
+
+class TypePsy : public Type {
+public:
+    TypePsy() : Type("Psy") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Insecte"] = 2.0;
+        faiblesses["Spectre"] = 2.0;
+        faiblesses["Ténèbres"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Combat"] = 0.5;
+        resistances["Psy"] = 0.5;
+    }
+};
+
+class TypeInsecte : public Type {
+public:
+    TypeInsecte() : Type("Insecte") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Feu"] = 2.0;
+        faiblesses["Vol"] = 2.0;
+        faiblesses["Roche"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Plante"] = 0.5;
+        resistances["Combat"] = 0.5;
+        resistances["Sol"] = 0.5;
+    }
+};
+
+class TypeRoche : public Type {
+public:
+    TypeRoche() : Type("Roche") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Eau"] = 2.0;
+        faiblesses["Plante"] = 2.0;
+        faiblesses["Combat"] = 2.0;
+        faiblesses["Sol"] = 2.0;
+        faiblesses["Acier"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Feu"] = 0.5;
+        resistances["Vol"] = 0.5;
+        resistances["Poison"] = 0.5;
+        resistances["Normal"] = 0.5;
+    }
+};
+
+class TypeSpectre : public Type {
+public:
+    TypeSpectre() : Type("Spectre") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Spectre"] = 2.0;
+        faiblesses["Ténèbres"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Poison"] = 0.5;
+        resistances["Insecte"] = 0.5;
+    }
+};
+
+class TypeDragon : public Type {
+public:
+    TypeDragon() : Type("Dragon") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Glace"] = 2.0;
+        faiblesses["Dragon"] = 2.0;
+        faiblesses["Fée"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Feu"] = 0.5;
+        resistances["Eau"] = 0.5;
+        resistances["Électrik"] = 0.5;
+        resistances["Plante"] = 0.5;
+    }
+};
+
+class TypeTenebres : public Type {
+public:
+    TypeTenebres() : Type("Ténèbres") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Combat"] = 2.0;
+        faiblesses["Insecte"] = 2.0;
+        faiblesses["Fée"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Spectre"] = 0.5;
+        resistances["Psy"] = 0.5;
+        resistances["Ténèbres"] = 0.5;
+    }
+};
+
+class TypeAcier : public Type {
+public:
+    TypeAcier() : Type("Acier") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Feu"] = 2.0;
+        faiblesses["Combat"] = 2.0;
+        faiblesses["Sol"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Normal"] = 0.5;
+        resistances["Plante"] = 0.5;
+        resistances["Glace"] = 0.5;
+        resistances["Vol"] = 0.5;
+        resistances["Psy"] = 0.5;
+        resistances["Insecte"] = 0.5;
+        resistances["Roche"] = 0.5;
+        resistances["Dragon"] = 0.5;
+        resistances["Acier"] = 0.5;
+        resistances["Fée"] = 0.5;
+    }
+};
+
+class TypeFee : public Type {
+public:
+    TypeFee() : Type("Fée") {
+        defFaiblesses();
+        defResistances(); // on appelle nos fonctions
+    }
+
+    void defFaiblesses() override {
+        faiblesses["Poison"] = 2.0;
+        faiblesses["Acier"] = 2.0;
+    }
+
+    void defResistances() override {
+        resistances["Combat"] = 0.5;
+        resistances["Insecte"] = 0.5;
+        resistances["Ténèbres"] = 0.5;
+        resistances["Dragon"] = 0.5;
+    }
+};
 
 class Pokemon {
-
-protected:
-	std::string nom;
-	std::vector<std::string> types;
-	int HP; //on dit PV normalement, noob
-	std::string attaque;
-	int degats;
-	//faiblesse ??
-	
-
 public:
-	Pokemon(std::string nom,
-		//std::vector<std::string> types,
-		int HP,
-		std::string attaque,
-		int degats);
+    std::string nom;
 
+    std::vector<std::string> types; //vecteur qui prend 1 ou 2 noms de types
+
+    std::unordered_map<std::string, float> faiblesses;
+    std::unordered_map<std::string, float> resistances;
+
+    int HP;
+    std::string attaque;
+    int degats;
+
+    Pokemon(std::string nom,
+        std::vector<std::string> t,
+        int HP,
+        std::string attaque,
+        int degats) : nom(nom), types(t) , HP(HP), attaque(attaque), degats(degats) {
+
+
+
+    }
+
+    virtual void attaquer() {}
+
+
+    virtual void recevoireDegats() {}
 };
-
-class PokemonSimple : public Pokemon {
-private : 
-	std::string type;
-
-public : 
-	PokemonSimple(std::string nom ,
-		int HP ,
-		std::string attaque ,
-		int degats) : Pokemon(nom , HP , attaque , degats) , type(type) {}
-
-};
-
-class PokemonDouble : public Pokemon {
-private:
-	std::string type1;
-	std::string type2;
-
-public:
-	PokemonDouble(std::string nom,
-		int HP,
-		std::string attaque,
-		int degats) : Pokemon(nom, HP, attaque, degats),
-		type1(type1) , type2(type2) {}
-
-
-};
-
 
